1ea5be04e7e7fcd7def1c631879d99482f5c4c19
mordiaw <diaw@tanelhealth.com>
2025-03-04 12:26:52 +0000

    I apolog

---
+++ assets/vendor/src/utils/isMobile.js
@@ -0,0 +1,13 @@
+import global from './global';
+
+const {
+    navigator
+} = global;
+
+const mobileAgent = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+);
+
+export default function isMobile() {
+    return mobileAgent;
+}
